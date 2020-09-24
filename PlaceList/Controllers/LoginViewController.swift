90acd8827c8a97963681cf01bf0a8576b6107fbd
VladislavMiro <vladislavmiro@icloud.com>
2020-09-25 00:51:56 +0300

    Add sign up view and update costrains

--- PlaceList/Controllers/LoginViewController.swift
+++ PlaceList/Controllers/LoginViewController.swift
@@ -9,17 +9,25 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    private var textFieldPosition : CGFloat = 0.0
+    
     @IBOutlet weak var emailTextField: loginTextField!
     @IBOutlet weak var passwordTextField: loginTextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tapGesture()
+        
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        removeKeyboardObserver()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        addKeyboardObserver()
         self.navigationController?.navigationBar.isHidden = true
     }
 
@@ -33,21 +41,61 @@ class LoginViewController: UIViewController {
         
     }
     
-    private func tapGesture() {
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapNotOnKeyboard))
-        view.addGestureRecognizer(tapGesture)
-    }
-    
-    @objc private func tapNotOnKeyboard() {
-        view.endEditing(true)
-    }
 }
 //: Поднятие view на размер клавиатуры
 extension LoginViewController: UITextFieldDelegate {
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
+        textFieldPosition = textField.frame.origin.y + textField.frame.height * 0.5
+        print("\(textFieldPosition)")
+    }
     
-        if textField.frame.origin.y <= 50 {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        switch textField {
+        case emailTextField:
+            textField.resignFirstResponder()
+            passwordTextField.becomeFirstResponder()
+        case passwordTextField:
+            textField.resignFirstResponder()
+        default:
+            textField.resignFirstResponder()
+        }
+        
+        return false
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    private func addKeyboardObserver() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidHide(notification:)), name: UIResponder.keyboardDidHideNotification, object: nil)
+    }
+    
+    private func removeKeyboardObserver() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc private func keyboardWillShow(notification: Notification) {
+        guard let userInfo = notification.userInfo as? [String:Any],
+              let _ = (userInfo[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue
+        else { return }
+        
+        UIView.animate(withDuration: 0.4) {
+            self.view.transform = CGAffineTransform(translationX: 0.0, y: -self.textFieldPosition)
+        }
+    }
+    
+    @objc private func keyboardDidHide(notification: Notification) {
+        UIView.animate(withDuration: 0.1) {
+            self.view.transform = .identity
+        }
+    }
+    /*func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.frame.origin.y <= 55 {
             self.view.frame.origin.y -= textField.frame.origin.y
         }
     }
@@ -59,7 +107,7 @@ extension LoginViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
-    }
+    }*/
     
 }
 
--- PlaceList/Controllers/RegisterViewController.swift
+++ PlaceList/Controllers/RegisterViewController.swift
@@ -9,17 +9,136 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    private var textFieldPosition: CGFloat = 0.0
+    private let datePicker = UIDatePicker()
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var surnameTextField: UITextField!
+    @IBOutlet weak var birthdayTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        ShowDatePicker()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        removeKeyboardObserver()
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        addKeyboardObserver()
         self.navigationController?.navigationBar.isHidden = false
     }
     
+    @IBAction func signUpButton(_ sender: UIButton) {
+        if passwordTextField != confirmPasswordTextField {
+            passwordTextField.backgroundColor = .red
+            confirmPasswordTextField.backgroundColor = .red
+            passwordError()
+        }
+    }
+    
+    private func ShowDatePicker() {
+        datePicker.datePickerMode = .date
+        datePicker.locale = .current
+        guard let localeID = Locale.preferredLanguages.first else { return }
+        datePicker.locale = Locale(identifier: localeID)
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneAction))
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        toolBar.setItems([flexSpace, doneButton], animated: true)
+        birthdayTextField.inputAccessoryView = toolBar
+        birthdayTextField.inputView = datePicker
+    }
+    
+    @objc private func doneAction() {
+        birthdayTextField.resignFirstResponder()
+        emailTextField.becomeFirstResponder()
+        let formater = DateFormatter()
+        formater.dateFormat = "dd.MM.yyyy"
+        birthdayTextField.text = formater.string(from: datePicker.date)
+    }
+    
+    private func passwordError() {
+        let alert = UIAlertController(title: "Error", message: "Wrong password!", preferredStyle: .alert)
+        let OKButton = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(OKButton)
+        present(alert, animated: true)
+    }
     
 
+}
+
+extension RegisterViewController: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textFieldPosition = textField.frame.origin.y + textField.frame.height
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        switch textField {
+        case nameTextField:
+            textField.resignFirstResponder()
+            surnameTextField.becomeFirstResponder()
+        case surnameTextField:
+            textField.resignFirstResponder()
+            birthdayTextField.becomeFirstResponder()
+        case emailTextField:
+            textField.resignFirstResponder()
+            passwordTextField.becomeFirstResponder()
+        case passwordTextField:
+            textField.resignFirstResponder()
+            confirmPasswordTextField.becomeFirstResponder()
+        case confirmPasswordTextField:
+            textField.resignFirstResponder()
+        default:
+            textField.resignFirstResponder()
+        }
+        
+        return false
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    private func addKeyboardObserver() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidHide(notification:)), name: UIResponder.keyboardDidHideNotification, object: nil)
+    }
+    
+    private func removeKeyboardObserver() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc private func keyboardWillShow(notification: Notification) {
+        guard let userInfo = notification.userInfo as? [String:Any],
+              let keyboardSize = (userInfo[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue
+        else { return }
+        
+        let distanceBetweenTextFieldAndKeyboard = self.view.frame.height - textFieldPosition - keyboardSize.height
+        print("\(distanceBetweenTextFieldAndKeyboard)")
+        if distanceBetweenTextFieldAndKeyboard < 200 {
+            UIView.animate(withDuration: 0.4) {
+                    self.view.transform = CGAffineTransform(translationX: 0.0, y: -distanceBetweenTextFieldAndKeyboard)
+            }
+        }
+    }
+    
+    @objc private func keyboardDidHide(notification: Notification) {
+        UIView.animate(withDuration: 0.4) {
+            self.view.transform = .identity
+        }
+    }
+    
 }
